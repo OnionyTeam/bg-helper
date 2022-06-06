e724f305ac71b630080bc5aebd6547ba07d8ecdd
flyingpig233-cpu <1686079252@qq.com>
2022-06-06 11:32:34 +0800

    created string_concat.hpp to_string.hpp

---
+++ string_concat.hpp
@@ -0,0 +1,8 @@
+#ifndef _BGHELPER_STRING_CONCAT_H
+#define _BGHELPER_STRING_CONCAT_H
+
+namespace bg_helper {
+    
+} // bg_helper
+
+#endif //_BGHELPER_STRING_CONCAT_H
---
+++ to_string.hpp
@@ -0,0 +1,8 @@
+#ifndef _BGHELPER_TO_STRING_H
+#define _BGHELPER_TO_STRING_H
+
+namespace bg_helper {
+    
+} // bg_helper
+
+#endif //_BGHELPER_TO_STRING_H
