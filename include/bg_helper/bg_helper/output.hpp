093d7c96ec1762d10d4f106f8e58eebf3147522d
flyingpig233-cpu <1686079252@qq.com>
2022-07-03 13:43:15 +0800

    cleaned the code

---
+++ include/bg_helper/bg_helper/output.hpp
@@ -0,0 +1,67 @@
+#ifndef _BGHELPER_OUTPUT_H__
+#define _BGHELPER_OUTPUT_H__
+
+#include "string_concat.hpp"
+#include "to_string.hpp"
+#include <algorithm>
+#include <cstdio>
+#include <exception>
+#include <list>
+#include <ostream>
+#include <string>
+#include <utility>
+
+namespace bg_helper {
+
+template <Character char_type_t = bg_helper::char_type> class Format {
+  public:
+	const static size_t DEFUALT_SIZE = 512;
+
+  private:
+	std::basic_string<char_type_t> buffer;
+
+  public:
+	Format() { buffer.reserve(DEFUALT_SIZE); }
+	template <typename... Args> Format(const Args &...args) {
+		buffer.reserve(DEFUALT_SIZE);
+		buffer.append(std::move(bg_helper::concat(args...)));
+	}
+
+	template <typename T> Format &operator|(const T &t) {
+		buffer.append(bg_helper::to_string<char_type_t>(t));
+		return *this;
+	};
+
+	void clear() { buffer.clear(); }
+	const std::basic_string<char_type_t> &to_string() const { return buffer; }
+	std::basic_string<char_type_t> to_string() { return buffer; }
+
+	friend std::ostream &operator<<(std::ostream &os, const Format &f) {
+		os << f.to_string();
+		return os;
+	}
+};
+
+template <Character char_type_t = bg_helper::char_type, typename... Args>
+constexpr void print(const Args &...v) {
+	const auto buffer = concat<char_type_t>(v...);
+	if constexpr (std::is_same_v<char_type_t, char>) {
+		printf("%s", buffer.c_str());
+	} else {
+		wprintf(L"%ls", buffer.c_str());
+	}
+}
+
+template <Character char_type_t = bg_helper::char_type, typename... Args>
+constexpr void println(const Args &...v) {
+	const auto buffer = concat<char_type_t>(v...);
+	if constexpr (std::is_same_v<char_type_t, char>) {
+		printf("%s\n", buffer.c_str());
+	} else {
+		wprintf(L"%ls\n", buffer.c_str());
+	}
+}
+
+} // namespace bg_helper
+
+#endif // _BGHELPER_OUTPUT_H__
--- include/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "output.hpp"
-#include "to_string.hpp"
-#include <iostream>
-#include <map>
-
-using namespace bg_helper;
-
-class A {};
-int main() {}
